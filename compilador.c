609d37ef0ed307048d535e6985a3c041710a78e4
josecantero <josedejesuscantero@gmail.com>
2013-06-10 13:47:12 -0300

    compilador.c
    Tarea 2 - Analizador Léxico
    Curso: Compiladores y Lenguajes de Bajo de Nivel
    Alumnos : Jose de Jesus Cantero Maciel

---
+++ compilador.c
@@ -0,0 +1,57 @@
+/*
+ *  Tarea 2 - Analizador Léxico	
+ *	Curso: Compiladores y Lenguajes de Bajo de Nivel
+ *  Alumnos : Jose de Jesus Cantero Maciel
+ */ 
+
+#include "analizadorLexico.h"
+#include "analizadorSintactico.h"
+
+
+/************** Funcion principal ****************/
+int main(int argc,char* args[])
+{
+	// inicializar analizador lexico
+	int complex=0;
+
+	initTabla();
+	initTablaSimbolos();
+         //CODIGO COMPILADO EN WINDOWS Y FUNCIONA ============================= 
+         /*if (!(archivo=fopen("fuente.txt", "r")))
+		 {
+			printf("Archivo no encontrado.");
+			//exit(1);
+		 }            //Funcion parse
+		 else{
+            parse();
+            if(HayError==FALSE){
+               printf("\nNo fue encontrado error alguno\n");                                  
+            }
+            fclose(archivo);
+            system("pause");
+           	return 0;
+         }///=================================================================*/
+
+         if(argc > 1)
+	     {
+		 if (!(archivo=fopen(args[1],"rt")))
+		 //if (!(archivo=fopen("fuente.txt", "r")))
+		 {
+			printf("Archivo no encontrado.");
+			exit(1);
+		 }            
+		 //else{
+            parse();
+            if(HayError==FALSE){
+               printf("\nNo fue encontrado error alguno\n");                                  
+            }
+            fclose(archivo);
+            system("pause");
+           	return 0;
+         //}
+        }else{
+		 printf("Debe pasar como parametro el path al archivo fuente.");
+		 exit(1);
+	    }
+	    system("pause");
+}
